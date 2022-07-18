969500c7209cf1a87c68f1793c79816e8cc70a8b
arifian853 <arifiansaputra43@gmail.com>
2022-07-18 15:54:44 +0700

    Updates

---
+++ precache-manifest.e31c172553076c449da1d4138e21f384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d42f3a9c5c26a4f137b0",
+    "url": "/loginAuth-frontend-build/static/css/main.60d53acb.chunk.css"
+  },
+  {
+    "revision": "d42f3a9c5c26a4f137b0",
+    "url": "/loginAuth-frontend-build/static/js/main.d42f3a9c.chunk.js"
+  },
+  {
+    "revision": "f993ed0ea40465f97e7e",
+    "url": "/loginAuth-frontend-build/static/js/1.f993ed0e.chunk.js"
+  },
+  {
+    "revision": "078cb520d76cf06e68e3",
+    "url": "/loginAuth-frontend-build/static/js/2.078cb520.chunk.js"
+  },
+  {
+    "revision": "c79b69e07b5c2bbbc4e5",
+    "url": "/loginAuth-frontend-build/static/js/runtime~main.c79b69e0.js"
+  },
+  {
+    "revision": "d956610b09e588719eb0edd280f0688c",
+    "url": "/loginAuth-frontend-build/static/media/bg2.d956610b.jpg"
+  },
+  {
+    "revision": "6d850bdab6d829c409467039f08b9189",
+    "url": "/loginAuth-frontend-build/index.html"
+  }
+];
